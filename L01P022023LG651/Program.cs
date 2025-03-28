2f6d4d260aa2d74cc676305d89ea940a781a8fbd
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-03-27 18:51:02 -0600

    Agregar configuración del contexto de base de datos para Entity Framework Core

--- L01P022023LG651/Program.cs
+++ L01P022023LG651/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PracticaMVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<equiposDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
