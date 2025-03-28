1710651bfeb25884d4254a36b5c3e157785d915d
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-03-27 18:50:29 -0600

    Agregar referencias de paquetes de Entity Framework Core y crear el contexto de base de datos

---
+++ L01P022023LG651/Models/equiposDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext: DbContext
+    {
+        public equiposDbContext(DbContextOptions<equiposDbContext> options): base(options)
+        {
+        }
+    }
+}
